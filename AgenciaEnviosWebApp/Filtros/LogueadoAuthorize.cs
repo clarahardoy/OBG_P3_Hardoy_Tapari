fe97a6ea125e9d1b058f6143e2f11e8c8b44ef25
Clara Hardoy <clarahardoy@icloud.com>
2025-04-12 20:54:24 -0300

    FILTRO: LogueadoAuthorize

---
+++ AgenciaEnviosWebApp/Filtros/LogueadoAuthorize.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AgenciaEnviosWebApp.Filtros;
+
+public class LogueadoAuthorize : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        int? usuarioLogueadoId = context.HttpContext.Session.GetInt32("LogueadoId");
+        if (usuarioLogueadoId == null)
+        {
+            context.Result = new RedirectToActionResult("Login", "Usuario", null);
+        }
+        
+}
