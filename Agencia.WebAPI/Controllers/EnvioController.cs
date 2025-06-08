e69a87d4db217966b309a81dac0d70afe76ead70
Clara Hardoy <clarahardoy@icloud.com>
2025-06-08 11:48:57 -0300

    traer email del usuario de las claims

--- Agencia.WebAPI/Controllers/EnvioController.cs
+++ Agencia.WebAPI/Controllers/EnvioController.cs
@@ -1,4 +1,5 @@
-﻿using Agencia.DTOs.DTOs.EnvioDTO;
+﻿using System.Security.Claims;
+using Agencia.DTOs.DTOs.EnvioDTO;
 using Agencia.LogicaAplicacion.CasosUso.CUEnvio;
 using Agencia.LogicaAplicacion.ICasosUso.ICUAgencia;
 using Agencia.LogicaAplicacion.ICasosUso.ICUEnvio;
@@ -39,6 +40,18 @@ namespace Agencia.WebAPI.Controllers
                 return StatusCode(500, "Error inesperado, intente más tarde.");
             }
 
+        }
+        private string EmailDelUsuario()
+        {
+            string email = null;
+            //obtener mail del token
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var emailClaim = claimsIdentity.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email);
+                email = emailClaim.Value;
+            }
+            return email;
         }
     }
 }
